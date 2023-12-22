b6465a390ceefb624531cffd2dd4236fe2ecd645
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-12-22 11:59:56 +0000

    add physics and platform image

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,6 +10,19 @@ export default class Game extends Phaser.Scene
   }
   create() {
     this.add.image(240, 320, 'background');
-    this.add.image(240, 320, 'platform');
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0; i < 5; ++i){
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = platforms.create(x, y, 'platform')
+
+      /** @type {Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body
+      body.updateFromGameObject()
+    }
+    this.physics.add.image(240, 320, 'platform');
   }
 }
